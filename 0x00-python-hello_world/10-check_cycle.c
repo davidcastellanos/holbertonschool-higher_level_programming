666f0cb9a6896e9300a7e0ffcd3d46b743003e43
davidcastellanos <castellanos.johann@gmail.com>
2019-12-09 21:23:13 -0500

    [Update] Fixed loop and de-referencing the pointer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,2 +1,31 @@
-#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - function that checks if there is a cycle in linked list
+ * @list: ptr to linked list
+ *
+ * Description: function in C that checks if a singly linked list has a cycle in it.
+ * Return: 0 if there's not a cycle and 1 if there's a cycle
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *moderate = '\0', *swift = '\0';
 
+	if (!list)
+		return (0);
+
+	moderate = swift = list;
+
+	do
+	{
+		swift = swift->next;
+		moderate = (*moderate).next;
+		if (swift)
+			swift = swift->next;
+	}
+
+	while (swift && moderate != swift);
+		if (swift && moderate == swift)
+			return (1);
+
+return (0);
+}
