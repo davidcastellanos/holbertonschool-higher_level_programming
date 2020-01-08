1efd68f1ef708655851852d277515fd54c050880
davidcastellanos <castellanos.johann@gmail.com>
2020-01-08 17:12:00 -0500

    [Update] Fixed function and import process

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import traceback as tb
 def safe_print_integer_err(value):
-from sys import stderr
+ret = False
     try:
         print("{:d}".format(value))
-        return True
+        ret = True
