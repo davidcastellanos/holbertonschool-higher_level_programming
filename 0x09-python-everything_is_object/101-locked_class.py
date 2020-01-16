87ec79dcfaab7017f96843fa1471d6ff399d5f5d
davidcastellanos <castellanos.johann@gmail.com>
2020-01-15 23:27:15 -0500

    [Update] Added and fixed all files

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module implementing LockedClass class.
+LockedClass is a class that uses the __slots__
+"""
+
+
+class LockedClass:
+    """LockedClass demonstrates the use of the __slots__ attribute.
+    Attributes:
+        __slots__: List of strings which may serve as valid attribute
+            identifiers as in `self.__dict__`.
+    """
+    __slots__ = ['first_name']
