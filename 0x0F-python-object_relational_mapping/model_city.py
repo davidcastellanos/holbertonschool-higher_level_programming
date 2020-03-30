2440c79892b66a75e4ec5b2f648a22b941089052
davidcastellanos <castellanos.johann@gmail.com>
2020-03-29 20:14:36 -0500

    [Update] Fixed model_state, model_city and sqlachemy libraries

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Create table `states` using SQLAlchemy"""
+"""Create table `cities`"""
 
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
-    """Class representing the `states` table.
+class City(Base):
+    """Class representing the `cities` table.
     Columns:
         id (int): /NOT NULL/AUTO_INCREMENT/PRIMARY_KEY/
-        name (string): /VARCHAR(128)/NOT NULL/
+        name (str): /VARCHAR(128)/NOT NULL/
+        state_id (int): /NOT NULL/FOREIGN KEY/
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
 
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
