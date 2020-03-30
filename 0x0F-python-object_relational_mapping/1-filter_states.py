c92143bb8bd4edb60dd6cd5c76770ab62604cb48
davidcastellanos <castellanos.johann@gmail.com>
2020-03-29 19:32:06 -0500

    [Update] Fixed loop in row

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,9 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states "
                 "WHERE name LIKE 'N%' "
