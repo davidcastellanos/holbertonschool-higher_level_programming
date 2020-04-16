edfe1cffd791754e55897e464007920222149c00
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:56:41 -0500

    [Update] Fixed argv positions and Pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+"""
+Get the 10 most recent commits from the repository specified on
+the command line ordered most recent to oldest in the
+format: `<sha>: <author name>`
+"""
+from sys import argv
+from requests import get
 
+
+if __name__ == "__main__":
+    usr = 'https://api.github.com/repos/' + argv[2] + '/' + argv[1] + '/commits'
+    res = get(usr)
+    try:
+        out = res.json()
+    except:
+        print("Not a valid JSON")
+    else:
+        lst = []
+        for entry in out:
+            sha = entry['sha']
+            author = entry['commit']['author']['name']
+            date = entry['commit']['author']['date']
+            lst.append((sha, author, date))
+    lst.sort(key=lambda x: x[2], reverse=True)
+    for entry in lst[:10]:
+        print("{}: {}".format(entry[0], entry[1]))
