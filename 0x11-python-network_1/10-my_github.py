a7705ebc0948be81ef71d2da143f67c09ed97a12
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:47:16 -0500

    [Update] Fixed arguments ansd path

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+"""Takes my github credentials and uses gh api to display my id"""
 
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        j = r.json()
+        print(j.get('id'))
+    except ValueError as e:
+        print(e)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+"""Send a POST request to the specified URL in the variable 'q'"""
+from requests import post
+from sys import argv
 
+
+if __name__ == "__main__":
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+    r = post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        out = r.json()
+    except:
+        print("Not a valid JSON")
+    else:
+        if out:
+            print("[{}] {}".format(out['id'], out['name']))
+        else:
+            print("No result")
