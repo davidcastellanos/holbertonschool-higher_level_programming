bbc8c2bfff1b9f31dc95d189d006809630c01575
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:46:38 -0500

    [Update] Fixed permissions in file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""Requests from URL and returns body response"""
 
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        r = get(argv[1])
+        if r.status_code >= 400:
+            print('Error code: {}'.format(r.status_code))
+        else:
+            print(r.text)
+    except Exception as e:
+        print(e)
