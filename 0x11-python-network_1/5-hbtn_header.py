a3544f2816dc3c2d723bdf6b7abd0450ded74b7f
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:45:22 -0500

    [Update] Fixed and removed import

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""Requests from URL and returns X-R from https://intranet.hbtn.io/status"""
 
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        r = get(argv[1])
+        print(r.headers.get('X-Request-Id'))
+    except Exception as e:
+        print(e)
