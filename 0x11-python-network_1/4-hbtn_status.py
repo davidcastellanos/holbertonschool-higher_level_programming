e84900f2749961cf89bd57fae0d5d6052394f6de
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:44:41 -0500

    [Update] Fixed condition

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""Fetch URL using requests package"""
+from requests import get
 
+
+if __name__ == "__main__":
+    r = get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content.decode('utf-8')))
