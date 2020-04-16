4f02db7172745caea96d08fe807df9ff85cbea32
davidcastellanos <castellanos.johann@gmail.com>
2020-04-16 12:45:47 -0500

    [Update] Fixed shebang

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""Post an email address to the requested URL"""
+from requests import post
+from sys import argv
 
+
+if __name__ == "__main__":
+    r = post(argv[1], data={'email': argv[2]})
+    print(r.text)
