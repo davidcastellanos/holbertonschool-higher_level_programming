586161dd4c040252435176e2c84e1f802c7ad840
davidcastellanos <castellanos.johann@gmail.com>
2020-05-05 18:46:39 -0500

    [Update] Print 'c is fun' 1st argument number of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,5 +2,5 @@
 let x = parseInt(process.argv[2]);
 if (isNaN(x)) console.log('Missing number of occurrences');
 else {
-    for (let i = 0; i < x; i++) console.log('C is fun');
+  for (let i = 0; i < x; i++) console.log('C is fun');
 }
