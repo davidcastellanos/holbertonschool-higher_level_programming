98ff4707b5d1748b478b5a892435a9ae343c701d
davidcastellanos <castellanos.johann@gmail.com>
2020-05-05 18:47:01 -0500

    [Update] Draw square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 let n = parseInt(process.argv[2]);
 if (isNaN(n)) console.log('Missing size');
 else {
-    for (let i = 0; i < n; i++) {
-        console.log('X'.repeat(n));
-    }
+  for (let i = 0; i < n; i++) {
+    console.log('X'.repeat(n));
+  }
 }
