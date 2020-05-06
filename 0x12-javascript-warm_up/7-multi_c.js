08aa5cb5aa794d2a7ef82faccc09e8312b63ad82
davidcastellanos <castellanos.johann@gmail.com>
2020-05-05 19:15:56 -0500

    [Update] Fixed requeriment

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (isNaN(x)) console.log('Missing number of occurrences');
-else {
-  for (const i = 0; i < x; i++) console.log('C is fun');
+const argv = process.argv;
+const x = parseInt(argv[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
