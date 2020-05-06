ed1a9cccdf78a791571fad147573ed7e8f8ec794
davidcastellanos <castellanos.johann@gmail.com>
2020-05-05 19:38:21 -0500

    [Update] Fixed requeriment

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
-function fact (n) {
-  if (isNaN(n) || n <= 1) return 1;
-  return n * fact(n - 1);
+const argv = process.argv;
+const x = parseInt(argv[2]);
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  }
+  if (n === 0) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
-let n = parseInt(process.argv[2]);
-console.log(fact(n));
+
+console.log(factorial(x));
