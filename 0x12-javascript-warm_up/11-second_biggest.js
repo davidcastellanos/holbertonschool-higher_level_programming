c45996d531f561cb25c1897eaa604d821d9298bb
davidcastellanos <castellanos.johann@gmail.com>
2020-05-05 19:39:16 -0500

    [Update] Fixed requeriment

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((item) => parseInt(item));
-if (args.length <= 1) console.log(0);
-else {
-  let max = args[0];
-  let second = args[1];
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > max) {
-      second = max;
-      max = args[i];
-    } else if (args[i] > second) {
-      second = args[i];
-    }
+const nums = process.argv;
+
+function secondMax (array) {
+  if (array.length <= 3) {
+    return (0);
   }
-  console.log(second);
+
+  array = array.slice(2).map(n => parseInt(n));
+
+  const max = Math.max(...array);
+
+  const arr = array.filter(arr => arr < max);
+
+  return (Math.max(...arr));
 }
+
+console.log(secondMax(nums));
