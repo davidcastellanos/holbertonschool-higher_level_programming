96536ce37b0f5b1b2b8028bd47131dafd1c5cd71
davidcastellanos <castellanos.johann@gmail.com>
2020-05-12 18:54:39 -0500

    [Update] Query swapi for episode number

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const request = require('request');
-request.get('http://swapi.co/api/films/' + process.argv[2] + '/', (err, response, body) => {
-  if (err) console.log(err);
-  else if (response.statusCode === 200) {
+// prints the title of a SW movie where the eps # matches a given int
+require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(JSON.parse(body).title);
   }
 });
