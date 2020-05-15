f2930c72ca0ffb0b3f1f1b09802d3c756e174944
davidcastellanos <castellanos.johann@gmail.com>
2020-05-14 23:16:12 -0500

    [Update] fetched data of the URL

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,12 @@
+$.ajax({
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  type: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    let hi = 'hello';
+    if ($('HTML')[0].lang === 'fr') {
+      hi = data.hello;
+    }
+    $('DIV#hello').text(hi);
+  }
+});
